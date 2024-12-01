d40bcd05156943b9d20349cdb822b4bd2a04b1ae
Hansanghyeon <999@hyeon.pro>
2024-12-01 10:59:10 +0000

    feat: 한글함수를 사용하는 나에게는 react-hook 규칙이 필요없다. 제거

--- src/react.mjs
+++ src/react.mjs
@@ -1,5 +1,4 @@
 import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
@@ -7,16 +6,10 @@ export default tseslint.config({
   settings: { react: { version: '18.3' } },
   plugins: {
     react,
-    'react-hooks': reactHooks,
     'react-refresh': reactRefresh,
   },
   rules: {
     ...react.configs.recommended.rules,
     ...react.configs['jsx-runtime'].rules,
-    ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
   },
 })
